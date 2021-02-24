4f577c6fa53d876d91408fecce3aecb71c73e58d
ARKANYOTA <cnolan@hotmail.fr>
2021-02-24 19:26:30 +0100

    deplacement de get, ajout du ctrl C, creation d'une boucle

--- index.py
+++ index.py
@@ -1,12 +1,18 @@
 import os
-from Get import getkey
-
-def init():
-    x,y = os.get_terminal_size()
+from get import getkey as gk
+def getkey():
+    key = gk()
+    if key=="\x03":
+        exit()
+    else:
+        return key   
 
 def main():
-    init()
+    x,y = os.get_terminal_size()
     print(x,y)
+    while 1:
+        key = getkey()
+        print(key)
 
 if __name__ == "__main__":
     main()
